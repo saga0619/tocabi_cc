5a0f9dcafeb6b6c85e9d21101ff930e811407ecc
saga0619 <june992@gmail.com>
2022-08-24 14:45:18 +0900

    haptic_minong

--- include/cc.h
+++ include/cc.h
@@ -1,5 +1,11 @@
 #include "tocabi_lib/robot_data.h"
 #include "wholebody_functions.h"
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Vector3.h"
+#include <tf/tf.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
 
 
 class CustomController
@@ -14,15 +20,28 @@ public:
     void computeFast();
     void computePlanner();
     void copyRobotData(RobotData &rd_l);
+    void PublishHapticData();
+
+    void HapticPoseCallback(const geometry_msgs::PoseConstPtr &msg);
+    Eigen::Matrix3d Quat2rotmatrix(double q0, double q1, double q2, double q3);
+    float PositionMapping( float haptic_pos, int i);
 
     RobotData &rd_;
     RobotData rd_cc_;
 
     ros::NodeHandle nh_cc_;
     ros::CallbackQueue queue_cc_;
+    ros::Subscriber haptic_pose_sub_;
+    ros::Publisher haptic_force_pub_;
+    
+    Eigen::Vector3d haptic_pos_;
+    Eigen::Vector4d haptic_ori_;
+    Eigen::Matrix3d haptic_orientation_;
 
     //WholebodyController &wbc_;
     //TaskCommand tc;
+
+    double haptic_force_[3];
 
 private:
     Eigen::VectorQd ControlVal_;
--- src/cc.cpp
+++ src/cc.cpp
@@ -2,10 +2,12 @@
 
 using namespace TOCABI;
 
+
 CustomController::CustomController(RobotData &rd) : rd_(rd) //, wbc_(dc.wbc_)
 {
     nh_cc_.setCallbackQueue(&queue_cc_);
-
+    haptic_pose_sub_ = nh_cc_.subscribe("/haptic/pose", 100, &CustomController::HapticPoseCallback, this);
+    haptic_force_pub_ = nh_cc_.advertise<geometry_msgs::Vector3>("/haptic/force", 10);
     // example_subsciber = nh_cc_.subscribe("/some/topic",10,&CustomController::myCallback,this);
     ControlVal_.setZero();
 }
@@ -20,6 +22,16 @@ Eigen::VectorQd CustomController::getControl()
 //     tc = tc_;
 // }
 
+void CustomController::PublishHapticData()
+{
+    geometry_msgs::Vector3 force;
+    force.x = haptic_force_[0];
+    force.y = haptic_force_[1];
+    force.z = haptic_force_[2];
+
+    haptic_force_pub_.publish(force);
+}
+
 void CustomController::computeSlow()
 {
     //MODE 6,7,8,9 is reserved for cc
@@ -27,10 +39,20 @@ void CustomController::computeSlow()
 
     
     if (rd_.tc_.mode == 6)
-    {
+    {   
         double ang2rad = 0.0174533;
 
         static bool init_qp;
+        
+        static VectorQd init_q_mode6;
+
+        static Matrix3d rot_hand_init;
+        static Matrix3d rot_haptic_init;
+
+        static Vector3d pos_hand_init;
+        static Vector3d pos_haptic_init;
+
+
         if (rd_.tc_init)
         {
             init_qp = true;
@@ -38,6 +60,15 @@ void CustomController::computeSlow()
             std::cout << "mode 6 init!" << std::endl;
             rd_.tc_init = false;
             rd_.link_[COM_id].x_desired = rd_.link_[COM_id].x_init;
+
+            init_q_mode6 = rd_.q_;
+
+            rot_hand_init = rd_.link_[Right_Hand].rotm;
+            rot_haptic_init = haptic_orientation_;
+
+            pos_hand_init = rd_.link_[Right_Hand].xpos;
+            pos_haptic_init = haptic_pos_;
+
         }
 
         WBC::SetContact(rd_, rd_.tc_.left_foot, rd_.tc_.right_foot, rd_.tc_.left_hand, rd_.tc_.right_hand);
@@ -64,8 +95,18 @@ void CustomController::computeSlow()
         rd_.link_[Pelvis].SetTrajectoryQuintic(rd_.control_time_, rd_.tc_time_, rd_.tc_time_ + rd_.tc_.time, rd_.link_[Pelvis].xi_init, rd_.link_[Pelvis].x_desired);
         rd_.link_[Pelvis].SetTrajectoryRotation(rd_.control_time_, rd_.tc_time_, rd_.tc_time_ + rd_.tc_.time);
 
-        rd_.link_[Right_Hand].SetTrajectoryQuintic(rd_.control_time_, rd_.tc_time_, rd_.tc_time_ + rd_.tc_.time);
-        rd_.link_[Right_Hand].SetTrajectoryRotation(rd_.control_time_, rd_.tc_time_, rd_.tc_time_ + rd_.tc_.time);
+        Vector3d hand_pos_desired = haptic_pos_ + pos_hand_init - pos_haptic_init;
+
+        Matrix3d hand_rot_desired = rot_hand_init * rot_haptic_init.transpose() * haptic_orientation_;
+
+        rd_.link_[Right_Hand].SetTrajectoryQuintic(rd_.control_time_, rd_.tc_time_, rd_.tc_time_ + rd_.tc_.time, hand_pos_desired);
+        //rd_.link_[Right_Hand].SetTrajectoryRotation(rd_.control_time_, rd_.tc_time_, rd_.tc_time_ + rd_.tc_.time);
+        rd_.link_[Right_Hand].SetTrajectoryRotation(rd_.control_time_, rd_.tc_time_, rd_.tc_time_ + rd_.tc_.time, hand_rot_desired, false);
+
+        std::cout<<"pos"<<std::endl;
+        std::cout<<haptic_pos_<<std::endl;
+        std::cout<<"ori"<<std::endl;
+        std::cout<<haptic_orientation_<< std::endl;
 
         rd_.link_[Upper_Body].SetTrajectoryRotation(rd_.control_time_, rd_.tc_time_, rd_.tc_time_ + rd_.tc_.time);
 
@@ -106,7 +147,32 @@ void CustomController::computeSlow()
 
         torque_Task2 = ts_.torque_h_ + ts_.Null_task * ts1_.torque_h_ + ts_.Null_task * ts1_.Null_task * ts2_.torque_h_ + rd_.torque_grav;
 
-        rd_.torque_desired = WBC::ContactForceRedistributionTorque(rd_, torque_Task2);
+                    // rd_.torque_desired[i] = rd_.pos_kp_v[i] * (rd_.q_desired[i] - rd_.q_[i]) + rd_.pos_kv_v[i] * (zero_m[i] - rd_.q_dot_[i]);
+        VectorQd torque_pos_hold;
+
+        for (int i=0;i<MODEL_DOF;i++)
+        {
+            torque_pos_hold[i] = rd_.pos_kp_v[i] * (init_q_mode6[i] - rd_.q_[i]) + rd_.pos_kv_v[i] * ( - rd_.q_dot_[i]);
+        }
+
+
+        torque_pos_hold.segment(25,8).setZero();
+
+        VectorQd torque_right_arm;
+        
+        torque_right_arm.setZero();
+
+        torque_right_arm.segment(25,8) = WBC::ContactForceRedistributionTorque(rd_, torque_Task2).segment(25,8);
+
+        rd_.torque_desired = torque_pos_hold + torque_right_arm;
+        
+        std::cout <<"torque" << rd_.RH_CF_FT<< std::endl;
+
+        haptic_force_[0] = rd_.RH_CF_FT[0] * 0.1;
+        haptic_force_[1] = rd_.RH_CF_FT[1]*0.1;
+        haptic_force_[2] = rd_.RH_CF_FT[2]*0.1;
+
+        PublishHapticData();
 
         init_qp = false;
     }
@@ -124,6 +190,73 @@ void CustomController::computeFast()
     else if (rd_.tc_.mode == 7)
     {
     }
+}
+
+void CustomController::HapticPoseCallback(const geometry_msgs::PoseConstPtr &msg)
+{
+
+    float pos_x = CustomController::PositionMapping(msg -> position.x, 0);
+    float pos_y = CustomController::PositionMapping(msg -> position.y, 1);
+    float pos_z = CustomController::PositionMapping(msg -> position.z, 2);
+    float ori_x = CustomController::PositionMapping(msg -> orientation.x, 3);
+    float ori_y = CustomController::PositionMapping(msg -> orientation.y, 4);
+    float ori_z = CustomController::PositionMapping(msg -> orientation.z, 5);
+    float ori_w = CustomController::PositionMapping(msg -> orientation.w, 6);
+
+    // double posx = static_cast<double>(pos_x);
+    // double posy = static_cast<double>(pos_y); 
+    // double posz = static_cast<double>(pos_z);
+    double orix = static_cast<double>(ori_x);
+    double oriy = static_cast<double>(ori_y); 
+    double oriz = static_cast<double>(ori_z);
+    double oriw = static_cast<double>(ori_w);
+
+    haptic_pos_[0] = pos_x;
+    haptic_pos_[1] = pos_y; 
+    haptic_pos_[2] = pos_z;
+
+    haptic_orientation_ = CustomController::Quat2rotmatrix(orix, oriy, oriz, oriw);
+
+}
+
+float CustomController::PositionMapping(float haptic_val, int i)
+{
+    if (i == 0){
+        return -1 * (haptic_val + 0.051448) * 5.0 ;
+    }
+
+    else if(i == 1){
+        return -1 * (haptic_val + 0.000152) * 5.0;
+    }
+
+    else if (i == 2){
+        return (haptic_val - 0.007794) * 5.0;
+    }
+    else {
+     return haptic_val;
+    }
+    
+}
+
+Eigen::Matrix3d CustomController::Quat2rotmatrix(double q0, double q1, double q2, double q3)
+{
+    double r00 = 2 * (q0 * q0 + q1 * q1) - 1 ;
+    double r01 = 2 * (q1 * q2 - q0 * q3) ;
+    double r02 = 2 * (q1 * q3 + q0 * q2) ;
+
+    double r10 = 2 * (q1 * q2 + q0 * q3) ;
+    double r11 = 2 * (q0 * q0 + q2 * q2) - 1 ;
+    double r12 = 2 * (q2 * q3 - q0 * q1) ;
+
+    double r20 = 2 * (q1 * q3 - q0 * q2) ;
+    double r21 = 2 * (q2 * q3 + q0 * q1) ;
+    double r22 = 2 * (q0 * q0 + q3 * q3) - 1 ;
+
+    Eigen::Matrix3d rot_matrix;
+    rot_matrix << r00, r01, r02,
+                    r10, r11, r12,
+                    r20, r21, r22;
+    return rot_matrix;
 }
 
 void CustomController::computePlanner()
